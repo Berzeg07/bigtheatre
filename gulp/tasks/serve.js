d5289675140b8d8b76a28008ebc6327154377ebb
Aslan <berezg86@gmail.com>
2019-10-10 19:37:44 +0300

    Верстка корзины

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "comment.html"
+                index: "basket.html"
             }
         });
     });
