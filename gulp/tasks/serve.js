5ff7fc89170a7d444a166b261c388e0b80e2469e
Aslan <berezg86@gmail.com>
2019-10-10 20:09:37 +0300

    Верстка премьера

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "basket.html"
+                index: "premiera.html"
             }
         });
     });
