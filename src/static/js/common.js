d144ca058f9c2a62b042ca0981205e20c49f41cb
Aslan <berezg86@gmail.com>
2019-10-10 21:48:29 +0300

    Мобильное меню

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -37,6 +37,37 @@ $(document).ready(function() {
         });
     });
 
+    $('.burger').click(function() {
+        $('.mob-hidden').removeClass('active');
+        $('.menu-mobile').toggleClass('active');
+        $('body').toggeClass('hidden');
+    });
+    $('.menu-mobile .menu-mobile__close').click(function() {
+        $('.menu-mobile').removeClass('active');
+        $('body').removeClass('hidden');
+    });
+
+    $('.date-btn').click(function() {
+        $('.mob-hidden').removeClass('active');
+        $('.mobile-info').toggleClass('active');
+        $('body').toggeClass('hidden');
+    });
+    $('.mobile-info .mobile-info__close').click(function() {
+        $('.mobile-info').removeClass('active');
+        $('body').removeClass('hidden');
+    })
+
+    // fixed menu *
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() >= 35) {
+                $('.header-desktop__bottom').addClass('sticky');
+            } else {
+                $('.header-desktop__bottom').removeClass('sticky');
+            }
+        });
+    });
+
 
 
     // custom select *
@@ -156,6 +187,47 @@ $(document).ready(function() {
     // переключатель плюс месяц
     document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
         calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) + 1);
+    }
+
+    function calendarMob(id, year, month) {
+        var Dlast = new Date(year, month + 1, 0).getDate(),
+            D = new Date(year, month, Dlast),
+            DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
+            DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+            calendar = '<tr>',
+            month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+        if (DNfirst != 0) {
+            for (var i = 1; i < DNfirst; i++) calendar += '<td>';
+        } else {
+            for (var i = 0; i < 6; i++) calendar += '<td>';
+        }
+        for (var i = 1; i <= Dlast; i++) {
+            if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+                calendar += '<td class="today">' + i;
+            } else {
+                calendar += '<td>' + i;
+            }
+            if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+                calendar += '<tr>';
+            }
+        }
+        for (var i = DNlast; i < 7; i++) calendar += '<td> ';
+        document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+        document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
+        document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
+        document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+        if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) { // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+            document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td> <td> <td> <td> <td> <td> <td> ';
+        }
+    }
+    calendarMob("calendarMob", new Date().getFullYear(), new Date().getMonth());
+    // переключатель минус месяц
+    document.querySelector('#calendarMob thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+        calendarMob("calendarMob", document.querySelector('#calendarMob thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendarMob thead td:nth-child(2)').dataset.month) - 1);
+    }
+    // переключатель плюс месяц
+    document.querySelector('#calendarMob thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+        calendarMob("calendarMob", document.querySelector('#calendarMob thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendarMob thead td:nth-child(2)').dataset.month) + 1);
     }
 
 
